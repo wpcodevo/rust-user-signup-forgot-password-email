d1692ed7b64a848a135c036040f4b134d40de19d
wpcodevo <ziddah.edem@gmail.com>
2023-07-09 14:57:33 +0000

    updated

--- src/response.rs
+++ src/response.rs
@@ -1,6 +1,8 @@
 use chrono::prelude::*;
 use serde::Serialize;
 
+use crate::model::User;
+
 #[allow(non_snake_case)]
 #[derive(Debug, Serialize)]
 pub struct FilteredUser {
@@ -12,6 +14,23 @@ pub struct FilteredUser {
     pub verified: bool,
     pub createdAt: DateTime<Utc>,
     pub updatedAt: DateTime<Utc>,
+}
+
+impl FilteredUser {
+    pub fn new_user(user: &User) -> Self {
+        let created_at_utc: DateTime<Utc> = DateTime::from_utc(user.created_at.unwrap(), Utc);
+        let updated_at_utc: DateTime<Utc> = DateTime::from_utc(user.updated_at.unwrap(), Utc);
+        Self {
+            id: user.id.to_string(),
+            email: user.email.to_owned(),
+            name: user.name.to_owned(),
+            photo: user.photo.to_owned(),
+            role: user.role.to_owned(),
+            verified: user.verified,
+            createdAt: created_at_utc,
+            updatedAt: updated_at_utc,
+        }
+    }
 }
 
 #[derive(Serialize, Debug)]
